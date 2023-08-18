8de8104b4aafbad89c15f27517fce77191a52364
Bilal Qadri <bilalattari1409l@gmail.com>
2023-08-18 16:06:23 +0500

    FIRST COMMIT

---
+++ 01_basics/data_type_comparison.js
@@ -0,0 +1,9 @@
+// console.log("2" > 1); // true
+// console.log("02" > 1); // true
+
+// console.log("02" === 2); //false    checking tightly same value and data type also
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
