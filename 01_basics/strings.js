a9703135b7e4c40305e69fc0ab767fc28dfd98d1
Bilal Qadri <bilalattari1409l@gmail.com>
2023-08-19 19:37:17 +0500

    string mothods

---
+++ 01_basics/strings.js
@@ -0,0 +1,9 @@
+var name = "bilal"
+var addrress = "swl"
+
+console.log(`Name of student is ${name} and address of student is ${addrress}`);
+
+
+let email = new String();
+email = "  bilal@g  mail.com           "
+console.log(email.trimStart());
