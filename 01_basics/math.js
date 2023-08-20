6e0f410a93e7eaedfd9226be9e6bb15d038cef1b
Bilal Qadri <bilalattari1409l@gmail.com>
2023-08-20 18:51:01 +0500

    Math functions

--- 01_basics/math.js
+++ 01_basics/math.js
@@ -1,19 +1,19 @@
-// console.log(Math.floor(12.33)); //12
-// console.log(Math.ceil(12.33)); //13
-// console.log(Math.round(12.33)); //12
+console.log(Math.floor(12.33)); //12
+console.log(Math.ceil(12.33)); //13
+console.log(Math.round(12.33)); //12
 
 
-// console.log(Math.abs(-14)); //14
-// console.log(Math.min(14,4,34,22,12)); //4
-// console.log(Math.max(14,4,34,22,12)); //34
+console.log(Math.abs(-14)); //14
+console.log(Math.min(14,4,34,22,12)); //4
+console.log(Math.max(14,4,34,22,12)); //34
 
-// console.log(Math.pow(5,2)); //25
-
-
-// console.log(Math.sqrt(25)); //5
+console.log(Math.pow(5,2)); //25
 
 
-// console.log(Math.round((Math.random()*50)+50)); //always generated 50 to 99
+console.log(Math.sqrt(25)); //5
+
+
+console.log(Math.round((Math.random()*50)+50)); //always generated 50 to 99
 
 let min = 60
 let max = 70
