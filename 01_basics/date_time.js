a56ee7978dd64edc1336cf22355b334f32f2272a
Bilal Qadri <bilalattari1409l@gmail.com>
2023-08-20 22:18:41 +0500

    date and time

---
+++ 01_basics/date_time.js
@@ -0,0 +1,10 @@
+let myDate = new Date();
+
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toISOString());
+console.log(myDate.toUTCString());
+
+
+
+console.log(Date.now()); // in miliSeconds
