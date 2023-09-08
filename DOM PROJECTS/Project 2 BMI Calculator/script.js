cacc7f646bf53bc37e26ce54f3d04a925ee4438a
Bilal Qadri <bilalattari1409l@gmail.com>
2023-09-08 20:02:02 +0500

    Project 2 completed

--- DOM PROJECTS/Project 2 BMI Calculator/script.js
+++ DOM PROJECTS/Project 2 BMI Calculator/script.js
@@ -1,36 +1,3 @@
-# PROJECTS RELATED TO DOM (JS solutions)
-
-## Project 1 (Background color changer)
-
-```javascript
-let buttons = document.querySelectorAll('.buttons');
-let body = document.querySelector('body');
-
-buttons.forEach((button) => {
-  button.addEventListener('click', (event) => {
-    if (event.target.id === 'grey') {
-      body.style.backgroundColor = event.target.id;
-    }
-    if (event.target.id === 'white') {
-      body.style.backgroundColor = event.target.id;
-    }
-    if (event.target.id === 'blue') {
-      body.style.backgroundColor = event.target.id;
-    }
-    if (event.target.id === 'yellow') {
-      body.style.backgroundColor = event.target.id;
-    }
-    if (event.target.id === 'green') {
-      body.style.backgroundColor = event.target.id;
-    }
-    if (event.target.id === 'pink') {
-      body.style.backgroundColor = event.target.id;
-    }
-  });
-});
-```
-## Project 2 (BMI Calculator)
-```javascript
 // select full form
 const form = document.querySelector('form')
 
@@ -62,4 +29,3 @@ form.addEventListener('submit', (event) => {
         result.innerHTML = `<span>Result is: ${bmi}<br>${guide}</span>`
     }
 })
-```
