f435ef772b10422100a525987d6c3e7b242741fb
Bilal Qadri <bilalattari1409l@gmail.com>
2023-09-16 20:16:18 +0500

    Project 5 completed

---
+++ DOM PROJECTS/Project 5 Keyboard check/script.js
@@ -0,0 +1,23 @@
+let insert = document.getElementById('insert')
+
+document.addEventListener('keydown',(e)=>{
+    insert.innerHTML = `
+        <div class = 'color'>
+            <table>
+
+            <tr>
+            <th>Key</th>
+            <th>KeyCode</th>
+            <th>Code</th>
+            </tr>
+
+            <tr>
+            <td>${e.key === " " ? 'Space': e.key}</td>
+            <td>${e.keyCode}</td>
+            <td>${e.code}</td>
+            </tr>
+            
+        </table>
+        </div>
+    `
+})
