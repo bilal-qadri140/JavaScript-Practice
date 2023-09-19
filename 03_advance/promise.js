4381a1dc9394270b871a319c6d42c3b8880f13f0
Bilal Qadri <bilalattari1409l@gmail.com>
2023-09-19 21:30:10 +0500

    Promises

--- 03_advance/promise.js
+++ 03_advance/promise.js
@@ -1,44 +1,44 @@
 
 // // methode 1
-// const promiseOne = new Promise(function (resolve, reject) {
-//     // do async
-//     // DB call, Cryptographi, network call etc
+const promiseOne = new Promise(function (resolve, reject) {
+    // do async
+    // DB call, Cryptographi, network call etc
 
-//     setTimeout(() => {
-//         resolve()
-//         console.log('Asyn are done');
-//     }, 1000);
-// })
+    setTimeout(() => {
+        resolve()
+        console.log('Asyn are done');
+    }, 1000);
+})
 
-// promiseOne.then(function () {
-//     console.log('promise consumed!');
-// })
+promiseOne.then(function () {
+    console.log('promise consumed!');
+})
 
 
 // //method 2
 
-// new Promise(function (resolve, reject) {
-//     setTimeout(() => {
-//         console.log('async 2 done');
-//         resolve()
-//     }, 1000);
-// }).then(function () {
-//     console.log('promise 2 consumed');
-// })
+new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        console.log('async 2 done');
+        resolve()
+    }, 1000);
+}).then(function () {
+    console.log('promise 2 consumed');
+})
 
 
-// //method 3
+//method 3
 
-// const promiseThree = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         resolve({ name: 'bilal', email: 'bilal@example.com' })
-//     }, 1000)
-// })
+const promiseThree = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve({ name: 'bilal', email: 'bilal@example.com' })
+    }, 1000)
+})
 
-// promiseThree.then((data) => {
-//     console.log(data);
-//     console.log(data.name)
-// })
+promiseThree.then((data) => {
+    console.log(data);
+    console.log(data.name)
+})
 
 // method 4
 
@@ -61,3 +61,28 @@ promiseFour.then((data) => {
 }).finally(() => {
     console.log('Finally executed');
 })
+
+// method 5
+
+const promiseFive = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const error = false
+        if (!error) {
+            resolve({ email: 'bilal@gmail.com', password: '123' })
+        }
+        else {
+            reject('Something went wrong!')
+        }
+    }, 1000);
+})
+
+async function consumePromiseFive() {
+    try {
+        const data = await promiseFive
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumePromiseFive()
