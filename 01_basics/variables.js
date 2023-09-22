4cfe0840e711ab428aebb2f9deb181878dccd4d2
Bilal Qadri <bilalattari1409l@gmail.com>
2023-09-22 20:18:48 +0500

    variables

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,5 +1,32 @@
-const customerId = 123;
-let customerName = "Bilal Qadri";
-var customerCity = "Sahiwal";
+// const customerId = 123;
+// let customerName = "Bilal Qadri";
+// var customerCity = "Sahiwal";
 
-console.table([customerId,customerName,customerCity]);
+// // console.table([customerId,customerName,customerCity]);
+
+// (function f (){
+//     var a = b = 3
+// })();
+// // (function f (){
+// //     local = 'bilal'
+// // })()
+
+// console.log(a);
+// console.log(typeof b);
+// // console.log(typeof local);
+
+// function greet(){
+//     console.log('hello world');
+// }
+// console.log(typeof greet);
+// console.log(typeof greet());
+
+// // console.log(0/0);
+// // console.log(NaN == NaN);
+// // console.log(Object.is(NaN,NaN));
+
+// console.log(1+'1');
+// console.log('3'-1);
+
+console.log(null == undefined);
+console.log(null === undefined);
